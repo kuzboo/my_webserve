d265ccb1de79f2a6f4b5c0f41333ac61bdc82d04
kuzboo <841161756@qq.com>
2022-06-01 16:28:12 +0800

    定时器finish

--- include/lst_timer.h
+++ include/lst_timer.h
@@ -7,14 +7,62 @@
 #include<sys/epoll.h>
 #include<sys/types.h>
 #include<sys/stat.h>
+#include<arpa/inet.h>
 #include<errno.h>
 #include<time.h>
 #include<string.h>
 #include<assert.h>
 
+//定时器类
+class util_timer
+{
+public:
+    util_timer() : prev(NULL), next(NULL){}
+public:
+    time_t expire;                  //超时时间
+    void (*cb_func)(client_data *); //回调函数
+    client_data *user_data;         //连接资源
+    util_timer *prev;               //前向定时器
+    util_timer *next;               //后向定时器
+};
 
+//连接资源结构体
+struct client_data
+{
+    sockaddr_in clnt_addr;//客户端地址
+    int sockfd;           //套接字文件描述符
+    util_timer *timer;    //定时器类
+};
 
+//定时器容器类
+class sort_timer_list
+{
+public:
+    sort_timer_list() : head(NULL), tail(NULL){}
+    ~sort_timer_list()
+    {
+        util_timer *s = head;
+        while(s)
+        {
+            head = s->next;
+            delete s;
+            s = head;
+        }
+    }
 
+    void add_timer(util_timer *timer);    //添加定时器，内部调用私有的add_timer;
+    void adjust_timer(util_timer *timer); //调整定时器，任务发生变化时，调整位置
+    void del_timer(util_timer *timer);    //删除定时器;
+    void tick();                          //定时任务处理函数;
+
+private:
+    //私有成员，被add_timer和adjust_time调用
+    void add_timer(util_timer *timer, util_timer *lst_head);
+    util_timer *head;
+    util_timer *tail;
+};
+
+//信号类
 class Utils
 {
 public:
@@ -22,15 +70,20 @@ public:
     ~Utils(){};
 
     void init(int timeslot);
-    //信号处理函数
-    static void sig_handler(int sig);
-    //设置信号函数
-    void addsig(int sig, void(handler)(int), bool restart = true);
+    int setnonblocking(int fd);                                    //将文件描述符设置成非阻塞;
+    static void sig_handler(int sig);                              //信号处理函数;
+    void addsig(int sig, void(handler)(int), bool restart = true); //设置信号函数;
+    void timer_handler();                                          //定时处理任务 重新定时以不断出发SIGALRM信号
+
+    //向内核事件表注册读事件,ET模式,开启EPOLLONESHOT (触发trigger)
+    void addfd(int epollfd, int fd, bool one_shot, int TRIGMode);
+
+    void show_error(int connfd, const char *info);
 
 public:
-    static int *u_pipefd;
-    static int u_epollfd;
+    static int *u_pipefd; 
+    static int u_epollfd; 
     int m_TIMESLOT;
 };
-
+void cd_func(client_data *user_data); //定时事件删除非活动连接
 #endif
---
+++ src/lst_timer.cpp
@@ -0,0 +1,162 @@
+#include"../include/lst_timer.h"
+#include"../include/http_conn.h"
+using namespace std;
+
+//自定义信号处理函数，创建sigaction结构体变量，设置信号函数。
+void Utils::sig_handler(int sig)
+{
+    //为保证函数的可重入性，保留原来的errno
+    //可重入性标识中断后再次进入该函数，环境变量与之前相同不会丢失数据
+    int save_errno = errno;
+    int msg = sig;
+
+    //将信号从管道写端写入，传输字符类型，而非整型
+    send(u_pipefd[1], (char *)&msg, 1, 0);
+
+    //将原来的errno赋值为当前的errno
+    errno = save_errno;
+}
+//注册信号函数(内部调用sigaction)
+void Utils::addsig(int sig,void(handler)(int),bool restart=true)
+{
+    //创建并初始化结构体变量
+    struct sigaction sa;
+    memset(&sa, '\0', sizeof(sa));//初始化为空字符
+
+    //信号处理函数紧紧发送信号值 不做对应逻辑处理
+    sa.sa_handler = handler;
+    if(restart)
+        sa.sa_flags |= SA_RESTART;//使被信号打断的系统调用自动重新发起
+    sigfillset(&sa.sa_mask);      //将所有信号添加到信号集中
+
+    assert(sigaction(sig, &sa, NULL) != -1);
+}
+//定时事件
+void cb_func(client_data* user_data)
+{
+    //删除非活动连接在socket上的注册事件
+    epoll_ctl(Utils::u_epollfd, EPOLL_CTL_DEL, user_data->sockfd, NULL);
+    assert(user_data);
+
+    close(user_data->sockfd);  //关闭文件描述符
+    http_conn::m_user_count--; //减少连接数
+}
+
+/*定时器容器类*/
+
+//添加定时器
+void sort_timer_list::add_timer(util_timer*timer,util_timer*lst_head)
+{
+    util_timer *prev_node = lst_head;
+    util_timer *tmp = prev_node->next;
+
+    //遍历当前结点之后的链表，按照超时时间找到目标定时器对应的位置，插入结点
+    while(tmp)
+    {
+        if(timer->expire<tmp->expire)
+        {
+            prev_node->next = timer;
+            timer->next = tmp;
+            tmp->prev = timer;
+            timer->prev = prev_node;
+            break;
+            }
+        prev_node = tmp;
+        tmp = tmp->next;
+    }
+
+    //如果定时器需要放到尾结点处
+    if(!tmp)
+    {
+        prev_node->next = timer;
+        timer->prev = prev_node;
+        timer->next = NULL;
+        tail = timer;
+    }
+}
+
+
+void sort_timer_list::adjust_timer(util_timer *timer)
+{
+    if (!timer)
+    {
+        return;
+    }
+    util_timer *tmp = timer->next;
+    if (!tmp || (timer->expire < tmp->expire))
+    {
+        return;
+    }
+    if (timer == head)
+    {
+        head = head->next;
+        head->prev = NULL;
+        timer->next = NULL;
+        add_timer(timer, head);
+    }
+    else
+    {
+        timer->prev->next = timer->next;
+        timer->next->prev = timer->prev;
+        add_timer(timer, timer->next);
+    }
+}
+void sort_timer_list::del_timer(util_timer *timer)
+{
+    if (!timer)
+    {
+        return;
+    }
+    if ((timer == head) && (timer == tail))
+    {
+        delete timer;
+        head = NULL;
+        tail = NULL;
+        return;
+    }
+    if (timer == head)
+    {
+        head = head->next;
+        head->prev = NULL;
+        delete timer;
+        return;
+    }
+    if (timer == tail)
+    {
+        tail = tail->prev;
+        tail->next = NULL;
+        delete timer;
+        return;
+    }
+    timer->prev->next = timer->next;
+    timer->next->prev = timer->prev;
+    delete timer;
+}
+
+//定时任务处理函数
+void sort_timer_list::tick()
+{
+    if(!head)
+        return;
+
+    time_t cur_time = time(NULL); //获取当前时间
+    util_timer *tmp = head;
+
+    //遍历定时器链表
+    while(tmp)
+    {
+        //当前时间小于定时器的超时时间，后面的定时器也没有到期
+        if (cur_time < tmp->expire)
+            break;
+        
+        //当前定时器到期，则调用回调函数，执行定时事件
+        tmp->cb_func(tmp->user_data);
+
+        //处理后的定时器从链表删除 并重置头节点
+        head = tmp->next;
+        if(head)
+            head->prev = NULL;
+        delete tmp;
+        tmp = head;
+    }
+}
